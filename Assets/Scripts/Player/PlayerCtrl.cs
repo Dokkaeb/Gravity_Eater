9b749b1ab8473395c60b06e70af152de289949ed
Dokkaeb <yj4705@gmail.com>
2026-01-13 18:08:29 +0900

    [feat] 대시기능 추가

--- Assets/Scripts/Player/PlayerCtrl.cs
+++ Assets/Scripts/Player/PlayerCtrl.cs
@@ -15,6 +15,10 @@ public class PlayerCtrl : MonoBehaviourPun
     [SerializeField] float _scaleMultiplier = 0.1f; //점수당 크기증가 비율
     [SerializeField] float _minScale = 1.0f;
 
+    [Header("대시 세팅")]
+    [SerializeField] float _dashMultiplier = 2.0f;
+    [SerializeField] float _dashCostPerSecond = 2.0f;
+
     Vector2 _mousePos;
     Rigidbody2D _rb;
 
@@ -30,11 +34,12 @@ public class PlayerCtrl : MonoBehaviourPun
         switch( _currentState)
         {
             case PlayerState.Move:
-                HandleMovement();
-                break;
-            case PlayerState.Dead:
+                HandleMovement(_moveSpeed);
                 break;
             case PlayerState.Dash:
+                HandleDash();
+                break;
+            case PlayerState.Dead:
                 break;
         }
     }
@@ -46,17 +51,31 @@ public class PlayerCtrl : MonoBehaviourPun
         Vector2 screenPos = ctx.ReadValue<Vector2>();
         _mousePos = Camera.main.ScreenToWorldPoint(screenPos);
 
-        if(_currentState != PlayerState.Dead)
+        if(_currentState != PlayerState.Dead && _currentState != PlayerState.Dash)
         {
             _currentState = PlayerState.Move;
+        }
+    }
+    public void OnDashInput(InputAction.CallbackContext ctx)
+    {
+        if(!photonView.IsMine || _currentState == PlayerState.Dead) return;
+
+        if(ctx.started && _currentScore > 0)
+        {
+            ChangeState(PlayerState.Dash);
+        }
+        else if (ctx.canceled)
+        {
+            ChangeState(PlayerState.Move);
         }
     }
     public void ChangeState(PlayerState newState)
     {
         _currentState = newState;
+        Debug.Log("현재 상태: " + newState);
     }
-
-    private void HandleMovement()
+    //이동
+    private void HandleMovement(float speed)
     {
         //마우스 방향으로 이동
         Vector2 dir = (_mousePos - (Vector2)transform.position).normalized;
@@ -65,11 +84,31 @@ public class PlayerCtrl : MonoBehaviourPun
         //마우스가 플레이어에 가까우면 멈춤
         if(distance > 0.1f)
         {
-            _rb.linearVelocity = dir * _moveSpeed;
+            _rb.linearVelocity = dir * speed;
         }
         else
         {
             _rb.linearVelocity = Vector2.zero;
+        }
+    }
+    //대시
+    private void HandleDash()
+    {
+        if (_currentScore > 0)
+        {
+            _currentScore -= _dashCostPerSecond * Time.deltaTime;
+
+            if (_currentScore < 0) _currentScore = 0;
+
+            UpdateScale();
+
+            photonView.RPC(nameof(RPC_UpdateScore), RpcTarget.Others, _currentScore);
+
+            HandleMovement(_moveSpeed * _dashMultiplier);
+        }
+        else
+        {
+            ChangeState(PlayerState.Move);
         }
     }
 
