dc38688b03414bbd15676625c2dd17d73ad1f79f
Dokkaeb <yj4705@gmail.com>
2026-01-22 10:08:31 +0900

    [feat] 타이틀 연출 추가

---
+++ Assets/Scripts/UI/TitlePolishing.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class TitlePolishing : MonoBehaviour
+{
+    [SerializeField] float _moveRange = 30f;
+    [SerializeField] float _moveTime = 2f;
+    private void Start()
+    {
+        transform.DOLocalMoveY(transform.localPosition.y + _moveRange, _moveTime)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo);
+
+    }
+}
